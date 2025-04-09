59ff45bec488c08032f686a912104630848e86dd
spxc73 <1171622802@qq.com>
2025-04-10 00:20:40 +0800

    Go!

---
+++ src/main/java/com/ganggood/autowork/AutoWorkApplication.java
@@ -0,0 +1,13 @@
+package com.ganggood.autowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoWorkApplication.class, args);
+    }
+
+}
